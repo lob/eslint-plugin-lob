8b66539c783876612416a27f65e0b447afcb2f45
Robin Joseph <robin@lob.com>
2016-02-14 21:01:54 -0800

    fix(align-equals): consider MemberExpressions as well

--- test/rules/align-equals.test.js
+++ test/rules/align-equals.test.js
@@ -13,6 +13,11 @@ Tester.run('align-equals', Rule, {
     'let ab = require("ab");\nlet b  = require("b");',
     'let a  = require("a");\nlet ab = require("ab");',
     'let a = require("a");\n\nlet ab = require("ab");',
+    'let a = require("a").a;',
+    'let a = require("a").a;\nlet b = require("b").b;',
+    'let ab = require("ab").ab;\nlet b  = require("b").b;',
+    'let a  = require("a").a;\nlet ab = require("ab").ab;',
+    'let a = require("a").a;\n\nlet ab = require("ab").ab;',
     'let a = Factory.build("a");',
     'let a = Factory.build("a");\nlet b = Factory.build("b");',
     'let ab = Factory.build("ab");\nlet b  = Factory.build("b");',
@@ -59,6 +64,48 @@ Tester.run('align-equals', Rule, {
   }, {
     code: 'let ab = "ab";\nlet b = "b";\n\nlet c  = require("c");',
     errors: [{ message: 'Extra space before equals for variable \'c\'.', line: 4 }]
+  }, {
+    code: 'let a  = require("a").a;',
+    errors: [{ message: 'Extra space before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a   = require("a").a;',
+    errors: [{ message: 'Extra spaces before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a= require("a").a;',
+    errors: [{ message: 'Missing space before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a =  require("a").a;',
+    errors: [{ message: 'Extra space after equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a =   require("a").a;',
+    errors: [{ message: 'Extra spaces after equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a =require("a").a;',
+    errors: [{ message: 'Missing space after equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a  = require("a").a;\nlet b = require("b").b;',
+    errors: [{ message: 'Extra space before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a  = require("a").a;\nlet b  = require("b").b;',
+    errors: [
+      { message: 'Extra space before equals for variable \'a\'.', line: 1 },
+      { message: 'Extra space before equals for variable \'b\'.', line: 2 }
+    ]
+  }, {
+    code: 'let a = require("a").a;\nlet b  = require("b").b;',
+    errors: [{ message: 'Extra space before equals for variable \'b\'.', line: 2 }]
+  }, {
+    code: 'let ab = require("ab").ab;\nlet b = require("b").b;',
+    errors: [{ message: 'Missing space before equals for variable \'b\'.', line: 2 }]
+  }, {
+    code: 'let ab = require("ab").ab;\nlet b  = require("b").b;\n\nlet c  = require("c").c;',
+    errors: [{ message: 'Extra space before equals for variable \'c\'.', line: 4 }]
+  }, {
+    code: 'let ab = "ab";\nlet b = "b";\n\nlet c  = require("c").c;',
+    errors: [{ message: 'Extra space before equals for variable \'c\'.', line: 4 }]
+  }, {
+    code: 'let ab = require("ab").ab;\nlet b = require("b");',
+    errors: [{ message: 'Missing space before equals for variable \'b\'.', line: 2 }]
   }, {
     code: 'let a  = Factory.build("a");',
     errors: [{ message: 'Extra space before equals for variable \'a\'.', line: 1 }]
