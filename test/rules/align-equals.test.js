256dadc8fa595589afc0872e5cc5d8988fae21ac
Robin Joseph <robin@lob.com>
2016-08-27 17:20:23 -0700

    fix(align-equals): account for promisification when aligning

--- test/rules/align-equals.test.js
+++ test/rules/align-equals.test.js
@@ -23,6 +23,16 @@ Tester.run('align-equals', Rule, {
     'let ab = require("ab")();\nlet b  = require("b")();',
     'let a  = require("a")();\nlet ab = require("ab")();',
     'let a = require("a")();\n\nlet ab = require("ab")();',
+    'let a = Bluebird.promisifyAll(require("a"));',
+    'let a = Bluebird.promisifyAll(require("a"));\nlet b = Bluebird.promisifyAll(require("b"));',
+    'let ab = Bluebird.promisifyAll(require("ab"));\nlet b  = Bluebird.promisifyAll(require("b"));',
+    'let a  = Bluebird.promisifyAll(require("a"));\nlet ab = Bluebird.promisifyAll(require("ab"));',
+    'let a = Bluebird.promisifyAll(require("a"));\n\nlet ab = Bluebird.promisifyAll(require("ab"));',
+    'let a = Bluebird.promisify(require("a").a);',
+    'let a = Bluebird.promisify(require("a").a);\nlet b = Bluebird.promisify(require("b").b);',
+    'let ab = Bluebird.promisify(require("ab").ab);\nlet b  = Bluebird.promisify(require("b").b);',
+    'let a  = Bluebird.promisify(require("a").a);\nlet ab = Bluebird.promisify(require("ab").ab);',
+    'let a = Bluebird.promisify(require("a").a);\n\nlet ab = Bluebird.promisify(require("ab").ab);',
     'let a = Factory.build("a");',
     'let a = Factory.build("a");\nlet b = Factory.build("b");',
     'let ab = Factory.build("ab");\nlet b  = Factory.build("b");',
@@ -148,6 +158,84 @@ Tester.run('align-equals', Rule, {
     code: 'let ab = "ab";\nlet b = "b";\n\nlet c  = require("c")();',
     errors: [{ message: 'Extra space before equals for variable \'c\'.', line: 4 }]
   }, {
+    code: 'let a  = Bluebird.promisifyAll(require("a"));',
+    errors: [{ message: 'Extra space before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a   = Bluebird.promisifyAll(require("a"));',
+    errors: [{ message: 'Extra spaces before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a= Bluebird.promisifyAll(require("a"));',
+    errors: [{ message: 'Missing space before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a =  Bluebird.promisifyAll(require("a"));',
+    errors: [{ message: 'Extra space after equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a =   Bluebird.promisifyAll(require("a"));',
+    errors: [{ message: 'Extra spaces after equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a =Bluebird.promisifyAll(require("a"));',
+    errors: [{ message: 'Missing space after equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a  = Bluebird.promisifyAll(require("a"));\nlet b = Bluebird.promisifyAll(require("b"));',
+    errors: [{ message: 'Extra space before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a  = Bluebird.promisifyAll(require("a"));\nlet b  = Bluebird.promisifyAll(require("b"));',
+    errors: [
+      { message: 'Extra space before equals for variable \'a\'.', line: 1 },
+      { message: 'Extra space before equals for variable \'b\'.', line: 2 }
+    ]
+  }, {
+    code: 'let a = Bluebird.promisifyAll(require("a"));\nlet b  = Bluebird.promisifyAll(require("b"));',
+    errors: [{ message: 'Extra space before equals for variable \'b\'.', line: 2 }]
+  }, {
+    code: 'let ab = Bluebird.promisifyAll(require("ab"));\nlet b = Bluebird.promisifyAll(require("b"));',
+    errors: [{ message: 'Missing space before equals for variable \'b\'.', line: 2 }]
+  }, {
+    code: 'let ab = Bluebird.promisifyAll(require("ab"));\nlet b  = Bluebird.promisifyAll(require("b"));\n\nlet c  = Bluebird.promisifyAll(require("c"));',
+    errors: [{ message: 'Extra space before equals for variable \'c\'.', line: 4 }]
+  }, {
+    code: 'let ab = "ab";\nlet b = "b";\n\nlet c  = Bluebird.promisifyAll(require("c"));',
+    errors: [{ message: 'Extra space before equals for variable \'c\'.', line: 4 }]
+  }, {
+    code: 'let a  = Bluebird.promisify(require("a").a);',
+    errors: [{ message: 'Extra space before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a   = Bluebird.promisify(require("a").a);',
+    errors: [{ message: 'Extra spaces before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a= Bluebird.promisify(require("a").a);',
+    errors: [{ message: 'Missing space before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a =  Bluebird.promisify(require("a").a);',
+    errors: [{ message: 'Extra space after equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a =   Bluebird.promisify(require("a").a);',
+    errors: [{ message: 'Extra spaces after equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a =Bluebird.promisify(require("a").a);',
+    errors: [{ message: 'Missing space after equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a  = Bluebird.promisify(require("a").a);\nlet b = Bluebird.promisify(require("b").b);',
+    errors: [{ message: 'Extra space before equals for variable \'a\'.', line: 1 }]
+  }, {
+    code: 'let a  = Bluebird.promisify(require("a").a);\nlet b  = Bluebird.promisify(require("b").b);',
+    errors: [
+      { message: 'Extra space before equals for variable \'a\'.', line: 1 },
+      { message: 'Extra space before equals for variable \'b\'.', line: 2 }
+    ]
+  }, {
+    code: 'let a = Bluebird.promisify(require("a").a);\nlet b  = Bluebird.promisify(require("b").b);',
+    errors: [{ message: 'Extra space before equals for variable \'b\'.', line: 2 }]
+  }, {
+    code: 'let ab = Bluebird.promisify(require("ab").ab);\nlet b = Bluebird.promisify(require("b").b);',
+    errors: [{ message: 'Missing space before equals for variable \'b\'.', line: 2 }]
+  }, {
+    code: 'let ab = Bluebird.promisify(require("ab").ab);\nlet b  = Bluebird.promisify(require("b").b);\n\nlet c  = Bluebird.promisify(require("c").c);',
+    errors: [{ message: 'Extra space before equals for variable \'c\'.', line: 4 }]
+  }, {
+    code: 'let ab = "ab";\nlet b = "b";\n\nlet c  = Bluebird.promisify(require("c").c);',
+    errors: [{ message: 'Extra space before equals for variable \'c\'.', line: 4 }]
+  }, {
     code: 'let a  = require("a")()();',
     errors: [{ message: 'Extra space before equals for variable \'a\'.', line: 1 }]
   }, {
@@ -155,6 +243,12 @@ Tester.run('align-equals', Rule, {
     errors: [{ message: 'Missing space before equals for variable \'b\'.', line: 2 }]
   }, {
     code: 'let ab = require("ab")();\nlet b = require("b");',
+    errors: [{ message: 'Missing space before equals for variable \'b\'.', line: 2 }]
+  }, {
+    code: 'let ab = Bluebird.promisifyAll(require("ab"));\nlet b = require("b");',
+    errors: [{ message: 'Missing space before equals for variable \'b\'.', line: 2 }]
+  }, {
+    code: 'let ab = Bluebird.promisify(require("ab").ab);\nlet b = require("b");',
     errors: [{ message: 'Missing space before equals for variable \'b\'.', line: 2 }]
   }, {
     code: 'let a  = Factory.build("a");',
